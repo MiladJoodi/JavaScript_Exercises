1aa6075d6ab837fea666fd791748df5b828cf497
Milad Joodi <43522323+miladjoodi@users.noreply.github.com>
2023-05-14 00:05:24 +0330

    Delete OlderThan18 directory

--- OlderThan18/script.js
+++
@@ -1,8 +0,0 @@
-
-var userAge = Number(prompt("Please Enter Your Age?"));
-
-if(userAge > 18){
-    alert("You entered the site");
-}else{
-    alert("Sorry. Your age is less than 18 years Old");
-}
