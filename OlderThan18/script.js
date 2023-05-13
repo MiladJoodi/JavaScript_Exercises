8f7c294fb15feaeb83b7f3f39db656255221c057
Milad Joodi <43522323+miladjoodi@users.noreply.github.com>
2023-05-13 18:24:16 +0330

    Add files via upload
    This code checks whether the user is over 18 years old or not.

---
+++ OlderThan18/script.js
@@ -0,0 +1,8 @@
+
+var userAge = Number(prompt("Please Enter Your Age?"));
+
+if(userAge > 18){
+    alert("You entered the site");
+}else{
+    alert("Sorry. Your age is less than 18 years Old");
+}
