620c35a4e6316298511e2c2b06c6bc86b45c6940
Milad Joodi <43522323+miladjoodi@users.noreply.github.com>
2023-05-14 21:43:17 +0330

    Delete 04.Power Result directory

--- 04.Power Result/script.js
+++
@@ -1,7 +0,0 @@
-
-var firstNumber = Number(prompt("Please enter first number..."));
-var secondNumber = Number(prompt("Please enter second number..."));
-
-var powerResult = firstNumber ** secondNumber;
-
-alert("Power Result is: " + powerResult);
