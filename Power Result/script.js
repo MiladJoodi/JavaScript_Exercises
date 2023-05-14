25dff85f90cf2bf6997dbee23265373dcb165450
Milad Joodi <43522323+miladjoodi@users.noreply.github.com>
2023-05-14 21:47:26 +0330

    Add files via upload

---
+++ Power Result/script.js
@@ -0,0 +1,7 @@
+
+var firstNumber = Number(prompt("Please enter first number..."));
+var secondNumber = Number(prompt("Please enter second number..."));
+
+var powerResult = firstNumber ** secondNumber;
+
+alert("Power Result is: " + powerResult);
