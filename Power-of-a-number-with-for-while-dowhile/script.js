9d9bb4123f73453f098f381e780fd2ecae1d36af
Milad Joodi <43522323+miladjoodi@users.noreply.github.com>
2023-05-23 21:40:06 +0330

    Add files via upload

---
+++ Power-of-a-number-with-for-while-dowhile/script.js
@@ -0,0 +1,37 @@
+
+// **************** for ****************
+// var userNumber = Number(prompt("Enter your number"))
+// var userPower = Number(prompt("Enter power"))
+// var power = 1
+// for (var i = 0; i < userPower; i++) {
+//     power = power * userNumber
+// }
+// alert(power)
+
+
+
+
+// **************** while ****************
+// var userNumber = Number(prompt("Enter your number"))
+// var userPower = Number(prompt("Enter power"))
+// var power = 1
+// var i = 0;
+// while (i < userPower) {
+//     power = power * userNumber
+//     i++
+// }
+// alert(power)
+
+
+// **************** do while ****************
+var userNumber = Number(prompt("Enter your number"))
+var userPower = Number(prompt("Enter power"))
+var power = 1
+var i = 0;
+do {
+    power = power * userNumber
+    i++
+} while (i < userPower);
+alert(power)
+
+
