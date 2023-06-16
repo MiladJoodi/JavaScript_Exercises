e4498dfc9e34ed14979de89dde0d83286ec50812
Milad Joodi <43522323+miladjoodi@users.noreply.github.com>
2023-06-16 21:29:04 +0330

    Add files via upload

---
+++ ticket-project-ex/js/script.js
@@ -0,0 +1,40 @@
+﻿
+let allCountries = {
+    Iran: ['Tehran', 'Esfahan', 'Shiraz', 'Ahvaz', 'Kerman'],
+    Canada: ['Montral', 'Quesbec', 'Torento'],
+    USA: ['Virgina', 'California', 'Los Angles']
+}
+
+
+let userCountrySelect = document.querySelector('.countrySelect');
+let citySelect = document.querySelector('.citySelect');
+
+userCountrySelect.addEventListener('change', function () {
+
+    if (userCountrySelect.value === 'Please Select') {
+        citySelect.innerHTML = ''
+        citySelect.innerHTML += '<option>Select City</option>';
+
+    } else {
+        let mainCountyName = userCountrySelect.value
+        let mainCountryCities = allCountries[mainCountyName]
+
+        citySelect.innerHTML = ''
+
+        mainCountryCities.forEach(function (city) {
+            // console.log(city);
+
+            citySelect.innerHTML += '<option>' + city + '</option>';
+        })
+    }
+
+
+
+
+
+    // console.log(mainCountryCities);
+
+    // console.log(allCountries[userCountrySelect.value]);
+
+}
+)
