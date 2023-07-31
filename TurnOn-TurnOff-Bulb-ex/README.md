7d01a7b68e4922654522ffef6edd94669b5d4fc9
Milad Joodi <43522323+MiladJoodi@users.noreply.github.com>
2023-07-31 21:59:20 +0330

    Add files via upload

---
+++ TurnOn-TurnOff-Bulb-ex/README.md
@@ -0,0 +1,10 @@
+## Password Input
+
+
+
+
+### Cover:
+![](https://s29.picofile.com/file/8466605584/TurnOn_TurnOff_Bulb_ex.gif)
+
+My GitHub Account: [GitHub.com/MiladJoodi](https://github.com/miladjoodi)  
+**Note**: [You can go here to download a single folder or file from GitHub.com](https://minhaskamal.github.io/DownGit/#/home)
---
+++ TurnOn-TurnOff-Bulb-ex/script.js
@@ -0,0 +1,29 @@
+﻿
+const $ = document
+const button = $.querySelector('.btn')
+const show = $.querySelector('.show')
+const img = $.querySelector('.img')
+
+let status = true
+
+button.addEventListener('click', function(){
+    if(status){
+        img.style.filter = 'grayscale(1)'
+        button.style.background = 'linear-gradient(0deg, #00E626 0, #00D608 100%)'
+        button.innerHTML = 'Turn On'
+        status = false
+
+    }else{
+        img.style.filter = 'grayscale(0)'
+        button.style.background = 'linear-gradient(0deg, #E60C0C 0, #C40000 100%)'
+        button.innerHTML = 'Turn Off'
+        status = true
+    }
+})
+
+button.addEventListener('mousedown',function(){
+        button.style.transform = 'scale(0.9)'
+})
+button.addEventListener('mouseup',function(){
+    button.style.transform = 'scale(1)'
+})
