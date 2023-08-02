b024afe5062df609561096ffd7caa84530a0bd47
Milad Joodi <43522323+MiladJoodi@users.noreply.github.com>
2023-08-02 12:22:48 +0330

    Add files via upload

---
+++ Even-or-Odd-Number-Checker/README.md
@@ -0,0 +1,10 @@
+## Even or Odd Number Checker
+
+
+
+
+### Cover:
+![](https://s8.uupload.ir/files/even-or-odd-number-checker_4yjk.gif)
+
+My GitHub Account: [GitHub.com/MiladJoodi](https://github.com/miladjoodi)  
+**Note**: [You can go here to download a single folder or file from GitHub.com](https://minhaskamal.github.io/DownGit/#/home)
---
+++ Even-or-Odd-Number-Checker/script.js
@@ -0,0 +1,21 @@
+﻿
+const $ = document
+const checkBtn = $.querySelector('.btn')
+const input = $.querySelector('.input')
+const checkResult = $.querySelector('.checkResult')
+
+checkBtn.addEventListener('click', function () {
+    if (input.value % 2 == 0) {
+        checkResult.innerHTML = ''
+        checkResult.innerHTML = 'Even'
+        checkResult.style.color = 'green'
+    } else if (isNaN(input.value % 2)) {
+        checkResult.innerHTML = 'Wrong Number'
+        checkResult.style.color = 'orange'
+    }
+    else {
+        checkResult.innerHTML = ''
+        checkResult.innerHTML = 'Odd'
+        checkResult.style.color = 'red'
+    }
+})
