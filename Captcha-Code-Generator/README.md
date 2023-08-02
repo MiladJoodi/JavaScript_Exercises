34d3ea8fd1085f52f15f5b0eb0836e2c38f0fe26
Milad Joodi <43522323+MiladJoodi@users.noreply.github.com>
2023-08-02 15:20:44 +0330

    Add files via upload

---
+++ Captcha-Code-Generator/README.md
@@ -0,0 +1,14 @@
+## Captcha Code Generator
+     Options ?:
+	? sensitive to Empty input
+	? sensitive to string input
+	? control range of 1~6  digit number
+
+
+
+
+### Cover:
+![](https://s8.uupload.ir/files/captcha-code-generator_9qfn.gif)
+
+My GitHub Account: [GitHub.com/MiladJoodi](https://github.com/miladjoodi)  
+**Note**: [You can go here to download a single folder or file from GitHub.com](https://minhaskamal.github.io/DownGit/#/home)
---
+++ Captcha-Code-Generator/script.js
@@ -0,0 +1,59 @@
+﻿
+const $ = document
+const checkBtn = $.querySelector('.btn')
+const input = $.querySelector('.input')
+const checkResult = $.querySelector('.checkResult')
+
+checkBtn.addEventListener('click', function () {
+
+
+    var userInput = input.value
+
+    switch (userInput) {
+        case '1':
+            userInput = 10
+            break;
+        case '2':
+            userInput = 100
+            break;
+        case '3':
+            userInput = 1000
+            break;
+        case '4':
+            userInput = 10000
+            break;
+        case '5':
+            userInput = 100000
+            break;
+        case '6':
+            userInput = 1000000
+            break;
+
+        default:
+            break;
+    }
+
+    if(isNaN(userInput)){
+        checkResult.innerHTML = 'Wrong Number!'
+        checkResult.style.color = 'red'
+        checkResult.style.textShadow = '5px 5px 5px rgba(0,0,0,0.3)'
+    }else if(userInput === ''){
+        checkResult.innerHTML = 'Empty!'
+        checkResult.style.color = 'orange'
+        checkResult.style.textShadow = '5px 5px 5px rgba(0,0,0,0.3)'
+    }else if(userInput<0){
+        checkResult.innerHTML = 'just 1~6'
+        checkResult.style.color = 'violet'
+        checkResult.style.textShadow = '5px 5px 5px rgba(0,0,0,0.3)'
+    }
+    else{
+        let randomNumber = Math.random() * userInput
+        randomNumber = Math.floor(randomNumber)
+    
+        checkResult.innerHTML = randomNumber
+        checkResult.style.color = 'green'
+        checkResult.style.textShadow = '5px 5px 5px rgba(0,0,0,0.3)'
+    }
+
+ 
+})
